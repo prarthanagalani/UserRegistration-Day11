b5734221fd2911b95efe8a9e39db60f2f716ec65
prarthanagalani <prarthanagalani@gmail.com>
2023-12-31 18:12:06 +0530

    added: tests for email validation

---
+++ ValidationTest.java
@@ -0,0 +1,47 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ValidationTest {
+
+    user u = new user("Prarthana", "Galani", "prarthanagalani@gmail.com", "91 9313935733", "Prar@1magd");
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "abc@yahoo.com",
+            "abc-100@yahoo.com",
+            "abc.100@yahoo.com",
+            "abc111@abc.com",
+            "abc-100@abc.net",
+            "abc.100@abc.com.au",
+            "abc@1.com",
+            "abc@gmail.com.com",
+            "abc+100@gmail.com"
+    })
+    public void parameterizedTestValidEmail(String validEmail) {
+        Assert.assertTrue(u.emailvalidate(validEmail));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "abc",
+            "abc@.com.my",
+            "abc123@gmail.a",
+            "abc123@.com",
+            "abc123@.com.com",
+            ".abc@abc.com",
+            "abc()*@gmail.com",
+            "abc@%*.com",
+            "abc..2002@gmail.com",
+            "abc.@gmail.com",
+            "abc@abc@gmail.com",
+            "abc@gmail.com.1a",
+            "abc@gmail.com.aa.au"
+    })
+    public void parameterizedTestInvalidEmail(String invalidEmail) {
+        Assert.assertFalse(u.emailvalidate(invalidEmail));
+    }
+
+}
